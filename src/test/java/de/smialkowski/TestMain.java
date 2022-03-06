214e52674bd81f15652cd29b0d58c5badead6e70
TobiasSMI <41923750+TobiasSMI@users.noreply.github.com>
2022-03-06 20:59:16 +0100

    Test angepasst

--- src/test/java/de/smialkowski/TestMain.java
+++ src/test/java/de/smialkowski/TestMain.java
@@ -1,5 +1,6 @@
 package de.smialkowski;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,7 @@ import org.junit.jupiter.api.Test;
 public class TestMain {
     @Test
     public void testInputIsEven() {
-        assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // Assertion
-        assertTrue(Main.checkIfInputIsAnEvenNumber(23)); // Assertion
+        assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // Test einer geraden Zahl, muss True ergeben
+        assertFalse(Main.checkIfInputIsAnEvenNumber(23)); // Test einer ungeraden Zahl, muss False ergeben
     }
 }
