11d2250519740593be68b058f352ff799d8b93b0
TobiasSMI <41923750+TobiasSMI@users.noreply.github.com>
2022-03-06 19:35:56 +0100

    Java Demo - init commit

---
+++ src/main/java/de/smialkowski/Main.java
@@ -0,0 +1,16 @@
+package de.smialkowski;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main(String[] args) {
+        System.out.println(checkIfInputIsAnEvenNumber(122)); // Testing in the main method
+    }
+
+    public static boolean checkIfInputIsAnEvenNumber(int number){
+        return number % 2 == 0;
+    }
+}
---
+++ src/test/java/de/smialkowski/TestMain.java
@@ -0,0 +1,15 @@
+package de.smialkowski;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestMain {
+    @Test
+    public void testInputIsEven() {
+        assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // Assertion
+    }
+}
